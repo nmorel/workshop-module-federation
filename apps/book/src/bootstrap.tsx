210e494b5434613c4935e7b21ebb35dc0c7b1609
Nicolas Morel <nmr.morel@gmail.com>
2022-09-30 09:39:25 +0200

    Fix standalone book app

--- apps/book/src/bootstrap.tsx
+++ apps/book/src/bootstrap.tsx
@@ -4,17 +4,20 @@ import {createRoot} from 'react-dom/client'
 import {Book} from './Book'
 import {MemoryRouter, Routes, Route} from 'react-router-dom'
 import {QueryProvider} from 'query-provider'
+import {books} from 'api'
 
 const container = document.getElementById('root')
 if (!container) {
   throw new Error('Element with id "root" not found, please define one in your HTML')
 }
 
+const [firstBook] = await books.getAll()
+
 const root = createRoot(container)
 root.render(
   <StrictMode>
     <QueryProvider>
-      <MemoryRouter initialEntries={['/books/around-the-world-in-eighty-days']}>
+      <MemoryRouter initialEntries={[`/books/${firstBook.slug}`]}>
         <Routes>
           <Route path="/books/:slug" element={<Book />} />
         </Routes>
