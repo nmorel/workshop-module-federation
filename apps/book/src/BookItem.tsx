e637486e3716096f0567c380c71ad913f995844b
Alban Depretz <alban.depretz@klaxoon.com>
2022-10-11 10:50:13 +0200

    Add specific highlighted behavior to book list item

--- apps/book/src/BookItem.tsx
+++ apps/book/src/BookItem.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react'
-import {Link} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
+import cx from 'classnames'
 
-export function BookItem({item}: {item: Bookshelf.Book}) {
+export function BookItem({item, highlighted}: {item: Bookshelf.Book; highlighted?: boolean}) {
+  const nav = useNavigate()
   return (
-    <Link
-      className="hover:underline"
-      to={{pathname: `/books/${item.slug}`, search: window.location.search}}
+    <button
+      type="button"
+      className={cx('py-2 px-4 w-full group flex justify-between', {
+        'bg-slate-100': !!highlighted,
+      })}
+      onClick={() => {
+        nav({pathname: `/books/${item.slug}`, search: window.location.search})
+      }}
     >
-      {item.title}
-    </Link>
+      <span className="group-hover:underline">{item.title}</span>
+      <span className="italic text-slate-500">{item.year}</span>
+    </button>
   )
 }
 
