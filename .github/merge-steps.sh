a8fb94c71b2a4cba740e635645d00d82bc92d5ba
Alban Depretz <alban.depretz@klaxoon.com>
2022-10-14 15:05:42 +0200

    Merge on origin instead

--- .github/merge-steps.sh
+++ .github/merge-steps.sh
@@ -1,89 +1,72 @@
-#!/bin/bash
+#!/bin/bash -e
 
 git fetch
 
 git checkout step-00
-git pull --rebase --autostash
 git push
 
 git checkout step-01
-git pull --rebase --autostash
-git merge step-00
+git merge origin/step-00 --no-edit
 git push
 
 git checkout step-01-result
-git pull --rebase --autostash
-git merge step-01
+git merge origin/step-01 --no-edit
 git push
 
 git checkout step-02
-git pull --rebase --autostash
-git merge step-01-result
+git merge origin/step-01-result --no-edit
 git push
 
 git checkout step-02-result
-git pull --rebase --autostash
-git merge step-02
+git merge origin/step-02 --no-edit
 git push
 
 git checkout step-03
-git pull --rebase --autostash
-git merge step-02-result
+git merge origin/step-02-result --no-edit
 git push
 
 git checkout step-03-result
-git pull --rebase --autostash
-git merge step-03
+git merge origin/step-03 --no-edit
 git push
 
 git checkout step-04
-git pull --rebase --autostash
-git merge step-03-result
+git merge origin/step-03-result --no-edit
 git push
 
 git checkout step-04-result
-git pull --rebase --autostash
-git merge step-04
+git merge origin/step-04 --no-edit
 git push
 
 git checkout step-05
-git pull --rebase --autostash
 git merge step-04-result
 git push
 
 git checkout step-05-result
-git pull --rebase --autostash
-git merge step-05
+git merge origin/step-05 --no-edit
 git push
 
 git checkout step-06
-git pull --rebase --autostash
-git merge step-05-result
+git merge origin/step-05-result --no-edit
 git push
 
 git checkout step-06-result
-git pull --rebase --autostash
-git merge step-06
+git merge origin/step-06 --no-edit
 git push
 
 git checkout step-07
-git pull --rebase --autostash
-git merge step-06-result
+git merge origin/step-06-result --no-edit
 git push
 
 git checkout step-07-result
-git pull --rebase --autostash
-git merge step-07
+git merge origin/step-07 --no-edit
 git push
 
 git checkout step-08
-git pull --rebase --autostash
-git merge step-07-result
+git merge origin/step-07-result --no-edit
 git push
 
 git checkout step-08-result
-git pull --rebase --autostash
-git merge step-08
+git merge origin/step-08 --no-edit
 git push
 
 git checkout step-00
