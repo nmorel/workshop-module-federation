4f3a2e311fb03c4494f495de15dc0d3ff1322b9d
Alban Depretz <alban.depretz@klaxoon.com>
2022-10-18 18:01:18 +0200

    Add App.tsx

--- apps/booklist/src/bootstrap.tsx
+++ apps/booklist/src/bootstrap.tsx
@@ -1,26 +1,19 @@
 import * as React from 'react'
-import {StrictMode} from 'react'
-import ReactDOM from 'react-dom'
-import {Booklist} from './Booklist'
-import {MemoryRouter, Routes, Route} from 'react-router-dom'
 import {QueryProvider} from 'query-provider'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {App} from './App'
 
 const container = document.getElementById('root')
 if (!container) {
   throw new Error('Element with id "root" not found, please define one in your HTML')
 }
 
-ReactDOM.render(
+const root = createRoot(container)
+root.render(
   <StrictMode>
     <QueryProvider>
-      <MemoryRouter initialEntries={['/']}>
-        <div className="px-8">
-          <Routes>
-            <Route path="/" element={<Booklist />} />
-          </Routes>
-        </div>
-      </MemoryRouter>
+      <App />
     </QueryProvider>
-  </StrictMode>,
-  container
+  </StrictMode>
 )
