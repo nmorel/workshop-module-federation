8312ecdec97a18d4d096774be8cd51864a269970
Nicolas Morel <nmr.morel@gmail.com>
2022-10-19 18:40:00 +0200

    Revert modif

--- apps/booklist/src/BooklistItem.tsx
+++ apps/booklist/src/BooklistItem.tsx
@@ -9,7 +9,7 @@ export function BooklistItem({item, className = ''}: {item: Bookshelf.Book; clas
       className={cx('py-2 px-4 w-full group flex justify-between', className)}
       to={{pathname: `/books/${item.slug}`, search: window.location.search}}
     >
-      <span className="group-hover:underline">{item.title}</span>
+      {item.title}
     </Link>
   )
 }
