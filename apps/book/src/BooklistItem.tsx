0127555c8aab38a2029ff0a2cf2a9b6ee04bbe94
Nicolas Morel <nmr.morel@gmail.com>
2022-10-19 18:35:42 +0200

    Simplify changes on BooklistItem

--- apps/book/src/BooklistItem.tsx
+++ apps/book/src/BooklistItem.tsx
@@ -3,14 +3,16 @@ import * as React from 'react'
 import {Link} from 'react-router-dom'
 import cx from 'classnames'
 
-export function BooklistItem({item, className = ''}: {item: Bookshelf.Book; className?: string}) {
+export function BooklistItem({book, index}: {book: Bookshelf.Book; index: number}) {
   return (
     <Link
-      className={cx('py-2 px-4 w-full group flex justify-between', className)}
-      to={{pathname: `/books/${item.slug}`, search: window.location.search}}
+      className={cx('py-2 px-4 w-full group flex justify-between', {
+        'bg-slate-100': index % 2 === 0,
+      })}
+      to={{pathname: `/books/${book.slug}`, search: window.location.search}}
     >
-      <span className="group-hover:underline">{item.title}</span>
-      <span className="italic text-slate-500">{item.year}</span>
+      <span className="group-hover:underline">{book.title}</span>
+      <span className="italic text-slate-500">{book.year}</span>
     </Link>
   )
 }
