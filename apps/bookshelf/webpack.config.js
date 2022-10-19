bb8b125a555a2a1f792a1d145432bbecaf9a2c7f
Nicolas Morel <nmr.morel@gmail.com>
2022-10-19 20:21:56 +0200

    Init MF plugin

--- apps/bookshelf/webpack.config.js
+++ apps/bookshelf/webpack.config.js
@@ -1,4 +1,5 @@
 const {createConfig} = require('webpack-config')
+const {ModuleFederationPlugin} = require('webpack').container
 
 module.exports = createConfig('Bookshelf', {
   output: {
@@ -8,6 +9,29 @@ module.exports = createConfig('Bookshelf', {
     port: 3000,
   },
   plugins: [
-    // Add Module Federation plugin configuration here
+    new ModuleFederationPlugin({
+      name: 'TODO',
+      remotes: {
+        TODO: 'TODO',
+      },
+      shared: {
+        'react': {
+          singleton: true,
+          requiredVersion: '18.1.0',
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: '18.1.0',
+        },
+        'react-router-dom': {
+          singleton: true,
+          requiredVersion: '^6.4.2',
+        },
+        'react-query': {
+          singleton: true,
+          requiredVersion: '^3.39.2',
+        },
+      },
+    }),
   ],
 })
